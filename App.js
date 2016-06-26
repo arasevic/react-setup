9f596af5ece1cb3734c5c1c14ff750270c4e49e8
Andrej Rasevic <andrej@rasevicengineering.com>
2016-06-26 14:04:28 -0400

    Added stylesheet and new react component

--- App.js
+++ App.js
@@ -1,8 +1,59 @@
 import React from 'react';
 class App extends React.Component {
-	render() {
-		return <div>Hello Nikola!!!! </div>
-	}
+	//render() {
+		// return <div>Hello Nikola!!!! </div>
+	//}
+//}
+
+ constructor() {
+    super();
+    
+    this.state = {
+      num: 0
+    }
+    
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.decrease}>-</button>
+        {this.state.num}
+        <button onClick={this.increase}>+</button>
+        
+        {this.state.num % 2 === 0 ?
+          <div>The number is even</div>
+          : null
+        }
+        
+        {this.state.num > 10 ?
+          <div>The number is greater than 10</div>
+          : null
+        }
+        
+      </div>
+    );
+  }
+  
+  increase() {
+    this.setState({
+      num: this.state.num + 1
+    })
+  }
+  
+  decrease() {
+    this.setState({
+      num: this.state.num - 1
+    })
+  }
 }
+
+//ReactDOM.render(
+ // <App />,
+  //document.getElementById('container')
+//);
 
 export default App
